43d6dd1b9680a33692c15408567957bf4ccdb47f
timius100 <timurbogdanov2008@gmail.com>
2022-09-23 23:48:55 +0300

    removed tokens (don't try them, they are resetted)

--- bot/variables.py
+++ bot/variables.py
@@ -1,16 +1,21 @@
 TEST_MODE = False
 
 if not TEST_MODE:
-    # кринжбот по Ху Тао
-    VK_GROUP_TOKEN = "vk1.a.iwPPxm779OLeQAU8LuTZbtuLWvN6BvfILWPKdLK-Xu4d6rXPmh-pMexWzl6EKAcDhTs3N91DdE9g3wkc2Gf9Eo-1w1XrctReYMI_R12a8yMR626l1N5d6D-doJPySs2HAHu06d4-CnHFVLfJVlbJiBSP0GXKkPfVtGO3S5cOS6lmvEvsXNE2aN_S219Zt64b"  # noqa: E501
-    GROUP_ID = 193964161
-else:
-    # абобус
-    VK_GROUP_TOKEN = "vk1.a.7Q-GKMiJgy2JRaXx8iFk5DjhO1-K38Q9FuMvSaoGVpqeLsM4O-QGSzw35Na1512I73QHbrooRhjst4v0ANUR7eaXPQjvGqHcU7JEQnL1lcgV5hR0T8s2ZwOkhFvX1k7P1-a5gT0HwYo4pNhmJuUyQyIpZAeMnXtPryxAN-E-Oerya-5pdydKzKqNTXtQ_AqH"  # noqa: E501
-    GROUP_ID = 205318987
+    # Here goes your main token
+    VK_GROUP_TOKEN = ""  # noqa: E501
 
-# я найду твой дом и взорву его, если сделаешь что-то с этим токеном
-VK_USER_TOKEN = "vk1.a.AEPoMI9faC1j53rcGxakSzD5ms9OWREtNE_ymA4fRkOqvDlhFgi_87RwuRHlUMP-PPFa65sGVCNSgGQL9ZB4oFPIOjwgd9yGnldGOYzxrHsFLYh4LmSajY-4e-JcNPqtZc2L1_Ws_QnkchKW5GZiuiikwdQHqZgk0GZvxoWVWuRewmTW9ZwmsVZpgntOucZt"  # noqa: E501
+    # Group id of your main group
+    GROUP_ID = 0
+else:
+    # Here goes your test group token
+    VK_GROUP_TOKEN = ""  # noqa: E501
+
+    # Group id of your test group
+    GROUP_ID = 0
+
+# Here goes your user token.
+# Kate Mobile tokens are guaranteed to work
+VK_USER_TOKEN = ""  # noqa: E501
 
 HELP_LINK = "https://vk.com/@we_love_hu_tao-start"
 EVENT_BANNER = "drifting_luminescence"
